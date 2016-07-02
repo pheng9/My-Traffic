163f05187e1c0082e832d8e1ac1e22c7cd75d0a8
Pheng <chanachai@addnet.co.th>
2016-07-02 15:00:31 +0700

    Finish 2016-07-02

--- app/src/main/java/thailand/cns/pheng/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/cns/pheng/mytraffic/MainActivity.java
@@ -1,13 +1,47 @@
 package thailand.cns.pheng.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Explicit การประกาศตัวแปร
+    // [Access] [Type] [Name]
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // Statement คือ คำสั่ง
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        // Controller for Button
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                mediaPlayer.start();
+
+                // Web VIew
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/CQbO1bDRTPA"));
+                startActivity(intent);
+
+            }   // onClick
+        });
+
+    }   // Main Method
+
+}   // Main Class นี่คือ คลาสหลัก
