afd4e5c062b0a03660a2feb9a7f08b1dd858d29d
Pheng <chanachai@addnet.co.th>
2016-07-03 14:04:05 +0700

    Show View Finish

--- app/src/main/java/thailand/cns/pheng/mytraffic/DetailActivity.java
+++ app/src/main/java/thailand/cns/pheng/mytraffic/DetailActivity.java
@@ -2,12 +2,48 @@ package thailand.cns.pheng.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    // Explicit
+    TextView nameTextView, detailTextView;  // การประกาศตัวแปรอย่างย่อ จะเป็น private อัตโนมัติ
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // Bind Widget
+        nameTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+
+        // Get Value from Intent
+        String strName = getIntent().getStringExtra("Name");
+        int intIcon = getIntent().getIntExtra("Icon", R.drawable.traffic_01);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        // Check Log
+        Log.d("Traffic", "strName ==> " + strName);
+        Log.d("Traffic", "intIcon ==> " + intIcon);
+        Log.d("Traffic", "intIndex ==> " + intIndex);
+
+        // Show View
+        nameTextView.setText(strName);
+        imageView.setImageResource(intIcon);
+
+        String[] detailStrings = getResources().getStringArray(R.array.long_data);
+        detailTextView.setText(detailStrings[intIndex]);
+
+    }   // Main Method
+
+    public void clickBack(View view) {
+        finish();
     }
-}
+
+}   // Main Class
